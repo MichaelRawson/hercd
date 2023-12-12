27ecf0df311d7eaca58cd4f7915199ac5e7df998
Michael Rawson <michael@rawsons.uk>
2023-12-12 14:15:46 +0000

    simplier graph caching

--- hercd/graph.py
+++ hercd/graph.py
@@ -14,10 +14,11 @@ class Node:
     C2 = 3
     ENTRY = 4
     GOAL = 5
-    END = 6
+    D = 6
+    END = 7
 
-CACHE: dict[tuple[int, ...], int] = {}
-"""cache for nodes"""
+CACHE: dict[F, int] = {}
+"""cache nodes representing formulae"""
 
 class Graph:
     """a graph object for representing CD states"""
@@ -44,8 +45,8 @@ class Graph:
         self.goal = str(goal)
 
         CACHE.clear()
-        self._node(Node.ENTRY, Node.ENTRY, self._formula(entry.formula))
-        self._node(Node.GOAL, Node.GOAL, self._formula(goal))
+        self._node(Node.ENTRY, self._formula(entry.formula))
+        self._node(Node.GOAL, self._formula(goal))
 
     def torch(self) -> BaseData:
         """produce a Torch representation of this graph"""
@@ -57,29 +58,31 @@ class Graph:
 
     def _formula(self, f: F) -> int:
         """add a formula to the graph"""
+
+        # cache previously-seen formulae
         if f in CACHE:
             return CACHE[f]
 
         if isinstance(f, int):
-            return self._node(Node.VAR, f)
+            node = self._node(Node.VAR)
         elif isinstance(f, N):
             negated = self._formula(f.negated)
-            return self._node(Node.N, Node.N, negated)
+            node = self._node(Node.N, negated)
         else:
             left = self._formula(f.left)
             right = self._formula(f.right)
-            left = self._node(Node.C2, Node.C2, left)
-            return self._node(Node.C1, Node.C1, left, right)
+            left = self._node(Node.C2, left)
+            node = self._node(Node.C1, left, right)
 
-    def _node(self, label: int, key: int, *children: int) -> int:
-        """add a node with `label` and `children`, cached by (key, *children)"""
-        if (key, *children) in CACHE:
-            return CACHE[key, *children]
+        CACHE[f] = node
+        return node
+
+    def _node(self, label: int, *children: int) -> int:
+        """add a node with `label` and `children`"""
 
         index = len(self.nodes)
         self.nodes.append(label)
         for child in children:
             self.sources.append(index)
             self.targets.append(child)
-        CACHE[key, *children] = index
         return index
